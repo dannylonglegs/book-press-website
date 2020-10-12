9d5747bbc9bb50b870c3d2c60d801c4654512e2f
dannylonglegs <ddanfernan@gmail.com>
2020-10-11 19:33:10 -0500

    General update

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,18 +15,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-    //   },
-    // },
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-shopify`,
@@ -34,7 +22,7 @@ module.exports = {
         // The domain name of your Shopify shop.
         shopName: `book-press-1.myshopify.com`,
         // The storefront access token
-        accessToken: `e3f18a0ab5e6103c6cbeda265683c17a`,
+        accessToken: process.env.SHOPIFY_ACCESS_TOKEN,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
